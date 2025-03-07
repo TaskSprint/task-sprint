dcac223a088048ae31917412d613fa707c8b5d7f
biguard1 <102917281+biguard1@users.noreply.github.com>
2025-03-07 18:58:14 +0000

    🖌️ Auto prettify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
-const {heroui} = require("@heroui/react");
+const { heroui } = require('@heroui/react');
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -10,7 +10,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.{js,ts,jsx,tsx}',
-        "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+        './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
 
     theme: {
