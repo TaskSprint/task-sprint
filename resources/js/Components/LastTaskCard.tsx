d25ffee4c4fa98e22b9763ef2e2414ca7a883a5e
0hmaxim <teran5656@gmail.com>
2025-03-25 19:34:10 +0100

    Small changes

--- resources/js/Components/LastTaskCard.tsx
+++ resources/js/Components/LastTaskCard.tsx
@@ -7,7 +7,7 @@ type TaskProps = {
     address: Record<string, any>;
     estimated_date: string;
     status: string;
-    customer: { id: number; name: string };
+    User: { id: number; name: string };
 };
 
 export default function LastTaskCard({
@@ -37,7 +37,7 @@ export default function LastTaskCard({
 
                 {/* Нижний блок с датой и ценой */}
                 <div className="flex flex-col items-start gap-[8px] w-[107px] h-[57px]">
-                    <p className="text-gray-400 font-manrope font-semibold text-[12px] leading-[16px]">
+                    <p className="text-gray-400 font-manrope font-semibold text-[12px] text-nowrap leading-[16px]">
                         Виконати до {estimated_date}
                     </p>
                     <p className="text-black font-manrope font-semibold text-[24px] leading-[33px]">
