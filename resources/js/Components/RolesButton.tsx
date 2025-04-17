e210c016c40741809f4ea0b20e5797aa732fbf39
pitvit87 <pit.vit@gmail.com>
2025-04-17 20:08:54 +0200

    Sizes of Roles buttons changed

--- resources/js/Components/RolesButton.tsx
+++ resources/js/Components/RolesButton.tsx
@@ -6,9 +6,9 @@ import { useLaravelReactI18n } from 'laravel-react-i18n';
 export default function Roles() {
     const { t } = useLaravelReactI18n();
     return (
-        <div className="flex w-[959px] h-[220px] py-[11px] ml-[20px] mt-[20px] gap-[36px] items-center justify-center flex-col">
+        <div className="flex w-full py-[0.625rem] ml-[1.25rem] mt-[1.25rem] gap-[2.25rem] items-center justify-center flex-col">
             <h2 className="text-[2rem] font-semibold items-center">{t('welcome.roles_question')}</h2>
-            <div className="flex justify-center w-[640px] h-[69px] gap-8">
+            <div className="flex justify-center w-[40rem] h-[4.30rem] gap-8">
                     <Button as={Link} href="#" className="text-white text-[1.25rem] font-semibold" color="primary">
                         {t('welcome.specialists')}
                     </Button>
