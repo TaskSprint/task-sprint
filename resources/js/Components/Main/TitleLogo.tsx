7670f6d41a1873bb5187bd75034f9480e7365df6
pitvit87 <pit.vit@gmail.com>
2025-04-22 11:11:54 +0200

    TitleLogo is Done!

--- resources/js/Components/Main/TitleLogo.tsx
+++ resources/js/Components/Main/TitleLogo.tsx
@@ -2,13 +2,23 @@ import { useLaravelReactI18n } from 'laravel-react-i18n';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import Button from '@/Components/Shared/Button';
 import { Link } from '@heroui/link';
+import {Image} from "@heroui/image";
+
 
 export function TitleLogo() {
     const { t } = useLaravelReactI18n();
 
     return (
-        <div className="flex flex-col w-full min-h-[26rem] justify-between gap-6 px-14 py-12
-            bg-[url(/images/title-logo.png)] bg-cover bg-center opacity-85 mt-20 ml-56 ">
+        <div className="flex flex-col relative w-full min-h-[26rem] justify-between gap-6 px-14 py-12
+            ">
+            <Image
+                alt="Title"
+                classNames={{wrapper: "size-full absolute -z-1 inset-0"}}
+                src="/images/title-logo.png"
+                className="size-full object-cover"
+                radius="none"
+                width="100%"
+            />
             <div  className="inline-flex h-9 items-center gap-2.5">
                 <ApplicationLogo
                     isFullSize
@@ -38,3 +48,21 @@ export function TitleLogo() {
         </div>
     );
 }
+
+
+/**
+ *
+ * import {Image} from "@heroui/react";
+ *
+ * export default function App() {
+ *   return (
+ *     <Image
+ *       alt="HeroUI hero Image"
+ *       src="https://heroui.com/images/hero-card-complete.jpeg"
+ *       width={300}
+ *     />
+ *
+ *
+ *
+ *
+ * /**/
