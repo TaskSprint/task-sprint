f59565ea558b78cc54c7d65c2f0ca916dcdc4710
pitvit87 <pit.vit@gmail.com>
2025-04-30 19:25:03 +0200

    Colorize Favorite

--- resources/js/Pages/Profile/Favorite.tsx
+++ resources/js/Pages/Profile/Favorite.tsx
@@ -13,7 +13,7 @@ export default function Favorite() {
     const absenceTime = 2; /*заглушка на время вместо CurrentTimeStamp - LastVisitTime*/
 
     return (
-        <div className="border-muted flex flex-col justify-between gap-3 border-b px-9 py-10 md:flex-row">
+        <div className="border-muted flex flex-col justify-between gap-3 border-b px-9 py-10 md:flex-row dark:bg-[#2c2c2c]">
             <div className="flex flex-col items-center gap-5 md:flex-row md:items-start">
                 <Link>
                     <Avatar
@@ -23,14 +23,14 @@ export default function Favorite() {
                 </Link>
                 <div className="flex flex-col items-center gap-5 md:items-start">
                     <div className="flex flex-row gap-2">
-                        <Link color="foreground" className="text-2xl font-bold">
+                        <Link color="foreground" className="text-2xl font-bold text-black dark:text-white focus:text-gray-500">
                             Коваль А.
                         </Link>
                         <p>
                             <HeartCheckbox />
                         </p>
                     </div>
-                    <h3 className="content-start text-base font-medium text-gray-500">
+                    <h3 className="content-start text-base font-medium text-[#929292] dark:text-[#A7A7A7]">
                         {t('fav-employees.last-visit', { absence: absenceTime } )}
                     </h3>
                 </div>
@@ -40,20 +40,21 @@ export default function Favorite() {
                     as={Link}
                     href="#"
                     variant="bordered"
-                    className="h-fit w-full rounded-full text-xl font-semibold md:px-10 md:py-5"
+                    className="h-fit bg-white dark:bg-[#2c2c2c] border-2 border-primary w-full
+                    rounded-full text-xl font-semibold md:px-10 md:py-5"
                     color="primary"
                 >
                     {t('fav-employees.offer')}
                 </Button>
                 <div className="flex flex-row items-center gap-2.5 font-medium">
                     <div className="flex flex-col">
-                        <h2 className="text-[2rem]">12456</h2>
+                        <h2 className="text-[2rem] dark:text-white">12456</h2>
                         <h3 className="text-primary text-xl">{t('fav-employees.reviews')}</h3>
                     </div>
                     <Divider className="bg-primary h-11" orientation="vertical" />
                     <div className="flex flex-col">
-                        <h2 className="text-[2rem]">100%</h2>
-                        <h3 className="text-xl text-gray-500">{t('fav-employees.positive')}</h3>
+                        <h2 className="text-[2rem] dark:text-white">100%</h2>
+                        <h3 className="text-xl text-gray-500 dark:text-[#A7A7A7]">{t('fav-employees.positive')}</h3>
                     </div>
                 </div>
             </div>
