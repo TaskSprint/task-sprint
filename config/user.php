f8830d193b9d491100ebdac31c9dacc74d30e49f
biguard1 <bogdasha05@gmail.com>
2025-05-01 21:39:42 +0300

    fix: updated config

--- config/user.php
+++ config/user.php
@@ -5,7 +5,7 @@ return [
         'name' => env('ADMIN_NAME', 'Admin'),
         'email' => env('ADMIN_EMAIL', 'admin@example.com'),
         'password' => env('ADMIN_PASSWORD', 'AdminExamplePassword'),
-        'phone' => env('ADMIN_PHONE', '1234567890'),
+        'phone' => env('ADMIN_PHONE', '+380951234567'),
         'city' => env('ADMIN_CITY', 'Admin City'),
     ],
 ];
