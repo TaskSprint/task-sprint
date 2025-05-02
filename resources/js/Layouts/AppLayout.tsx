3e5cd1029177f5612e9512f0404502d0379d5515
biguard1 <bogdasha05@gmail.com>
2025-05-02 06:46:56 +0300

    fix: and another little layout fix

--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -4,9 +4,9 @@ import { Footer } from '@/Components/Layout/Footer';
 
 export default function AppLayout({ children }: Readonly<PropsWithChildren>) {
     return (
-        <div className="grid min-h-screen w-screen grid-cols-[100dvw] grid-rows-[auto_1fr_auto]">
+        <div className="grid min-h-screen w-full grid-cols-1 grid-rows-[auto_1fr_auto]">
             <Navigation />
-            <main className="background-gradient flex min-h-[calc(100dvh-var(--spacing-navbar-height))] max-w-screen flex-col items-center justify-center overflow-x-hidden">
+            <main className="background-gradient flex min-h-[calc(100dvh-var(--spacing-navbar-height))] max-w-screen flex-col items-center justify-center overflow-x-auto">
                 {children}
             </main>
             <Footer />
