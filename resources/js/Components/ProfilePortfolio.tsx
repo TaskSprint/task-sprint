3e6792d8ace9b33b96e77d5cfa0f9cdc6bb62f98
pitvit87 <pit.vit@gmail.com>
2025-05-16 13:28:07 +0200

    + 2 ver

--- resources/js/Components/ProfilePortfolio.tsx
+++ resources/js/Components/ProfilePortfolio.tsx
@@ -1,16 +1,54 @@
-import Button from '@/Components/Shared/Button';
-import { useLaravelReactI18n } from 'laravel-react-i18n';
-import { Link } from '@heroui/react';
+import { useLaravelReactI18n } from 'laravel-react-i18n';import { Link } from '@heroui/react';
+import {Chip} from "@heroui/chip";
+import {Image} from "@heroui/image";
+
 
 export default function Support() {
     const { t } = useLaravelReactI18n();
 
     return (
-        <div className="flex flex-col items-center justify-center gap-9 px-2 py-28">
-            <h2 className="text-center text-[2rem] font-semibold">{t('main.any_questions')}</h2>
-            <h2 className="text-center text-[2rem] font-semibold">{t('main.any_questions')}</h2>
+        <div className="flex flex-col w-[44.625rem] min-h-[28.5625rem] items-start justify-start gap-6 p-4">
+            <div className="flex flex-col w-[42.625rem] min-h-[17.0625rem] items-start justify-start p-4 gap-8">
+                <h2 className="text-start text-[1.5rem] font-bold dark:text-white">{t('profile-portfolio.portfolio-title')}</h2>
+                    <div className="flex flex-row w-[40.625rem] h-[11rem] items-center gap-6">
 
+                        <Image
+                            isZoomed
+                            alt="HeroUI Fruit Image with Zoom"
+                            src="https://heroui.com/images/fruit-1.jpeg"
+                            className="w-[19.5625rem] h-[11rem]"
+                        />
+                        <Image
+                            isZoomed
+                            alt="HeroUI Fruit Image with Zoom"
+                            src="https://heroui.com/images/fruit-1.jpeg"
+                            className="w-[19.5625rem] h-[11rem]"
+                        />
+                    </div>
 
+                    <div className="flex flex-col gap-4">
+                        <h2 className="text-center text-[2rem] font-semibold">{t('profile-portfolio.portfolio-folder')}</h2>
+
+                        <div className="flex gap-4 size-7">
+                            <Chip
+                                color="primary"
+                                variant="bordered"
+                                size="lg"
+                                >
+                                UI-UX
+                            </Chip>
+                            <Chip
+                                color="primary"
+                                variant="bordered"
+                                size="lg"
+
+                            >
+
+                                web interface
+                            </Chip>
+                        </div>
+                    </div>
+            </div>
         </div>
     );
 }
