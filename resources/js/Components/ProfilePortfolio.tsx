c94210b24077ed0408a21b743a9dd773f7ac792f
biguard1 <bogdasha05@gmail.com>
2025-05-20 15:17:19 +0300

    fix: some style fixes

--- resources/js/Components/ProfilePortfolio.tsx
+++ resources/js/Components/ProfilePortfolio.tsx
@@ -1,50 +1,49 @@
 import { useLaravelReactI18n } from 'laravel-react-i18n';
-import {Image} from "@heroui/image";
+import { Image } from '@heroui/image';
 import { Chip } from '@heroui/react';
-import React from "react";
-
+import React from 'react';
 
 export default function ProfilePortfolio() {
     const { t } = useLaravelReactI18n();
     const folders = [
-        { id: 1, name: "UI-UX" },
-        { id: 2, name: "web interface" },
-        { id: 3, name: "javascript" },
+        { id: 1, name: 'UI-UX' },
+        { id: 2, name: 'web interface' },
+        { id: 3, name: 'javascript' },
     ];
 
-
-
     return (
-        <div className="flex w-[44.625rem] min-h-[26.5625rem] gap-6 p-4">
-            <div className="flex flex-col items-start w-[42.625rem] min-h-[17.0625rem]  p-4 gap-8">
-                <div className="flex flex-col w-[40.625rem] h-[11rem] gap-6">
+        <div className="flex gap-6 p-4">
+            <div className="flex flex-col items-start gap-8 p-4">
+                <div className="flex flex-col gap-6">
+                    <h2 className="flex flex-col text-start text-[1.5rem] font-bold dark:text-white">
+                        {t('profile-portfolio.portfolio-title')}
+                    </h2>
 
-                   <h2 className="flex flex-col text-start text-[1.5rem] font-bold dark:text-white">{t('profile-portfolio.portfolio-title')}</h2>
-
-                    <div className="flex flex-row gap-6">
+                    <div className="flex flex-wrap gap-6">
                         <Image
                             isZoomed
                             alt="HeroUI Fruit Image with Zoom"
                             src="https://heroui.com/images/fruit-1.jpeg"
-                            className="w-[19.5625rem] h-[11rem] cursor-pointer"
-
-                       />
+                            className="h-[11rem] w-[19.5625rem] cursor-pointer !transition"
+                        />
                         <Image
                             isZoomed
                             alt="HeroUI Fruit Image with Zoom"
                             src="https://heroui.com/images/fruit-1.jpeg"
-                            className="w-[19.5625rem] h-[11rem] cursor-pointer"
+                            className="h-[11rem] w-[19.5625rem] cursor-pointer !transition"
                         />
                     </div>
 
                     <div className="flex flex-col gap-4">
-                        <h2 className="flex flex-row text-start text-[1.5rem] font-bold dark:text-white">{t('profile-portfolio.portfolio-folder')}</h2>
+                        <h2 className="text-[1.5rem] font-bold">
+                            {t('profile-portfolio.portfolio-folder')}
+                        </h2>
 
-                        <div className="flex flex-row h-[3.1875rem] gap-6 ">
+                        <div className="flex flex-wrap gap-6">
                             {folders.map((folder) => (
                                 <Chip
                                     key={folder.id}
-                                    className="dark:!color-white cursor-pointer border-1 border-gray-500 dark:!border-gray-400"
+                                    className="dark:!color-white border-muted cursor-pointer border-1"
                                     variant="bordered"
                                     size="lg"
                                     radius="sm"
@@ -57,6 +56,6 @@ export default function ProfilePortfolio() {
                     </div>
                 </div>
             </div>
-       </div>
-      );
+        </div>
+    );
 }
--- resources/js/Pages/Main.tsx
+++ resources/js/Pages/Main.tsx
@@ -1,6 +1,6 @@
 import { Head } from '@inertiajs/react';
 
-import React, { Profiler } from 'react';
+import React from 'react';
 import Categories from '@/Components/Main/Categories';
 import { Info } from '@/Components/Main/Info';
 import LastTasks from '@/Components/Main/LastTasks/LastTasks';
@@ -11,7 +11,6 @@ import { Title } from '@/Components/Main/Title';
 import Category from '@/types/models/category';
 import { PageProps } from '@/types';
 import Task from '@/types/models/task';
-import ProfilePortfolio from '@/Components/ProfilePortfolio';
 
 export default function Main({
     categories,
@@ -25,9 +24,14 @@ export default function Main({
     return (
         <>
             <Head title={t('main.home')} />
-            <div className="bg-surface h-full w-full max-w-[60rem] border-l-2 mr-4">
-                <ProfilePortfolio />
 
+            <div className="bg-surface h-full w-full max-w-[60rem]">
+                <Title />
+                <Categories categories={categories} />
+                <Roles />
+                <Info />
+                <LastTasks tasks={tasks} />
+                <Support />
             </div>
         </>
     );
