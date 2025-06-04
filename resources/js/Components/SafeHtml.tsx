6eda85423e21be38739df836af51d937c3d6bfb8
biguard1 <bogdasha05@gmail.com>
2025-06-04 15:45:39 +0300

    fix: changed safe html

--- resources/js/Components/SafeHtml.tsx
+++ resources/js/Components/SafeHtml.tsx
@@ -13,19 +13,23 @@ const SafeHtml = forwardRef<ElementType, Omit<SafeHtmlProps, 'ref'>>(
     ({ className, html, ...props }, ref) => {
         return parse(DOMPurify.sanitize(html), {
             replace: (domNode) => {
-                const element = domNode as Element;
-                const Component = element.tagName as ElementType;
-                const attrs = attributesToProps(element.attribs);
-                return (
-                    <Component
-                        {...attrs}
-                        {...props}
-                        ref={ref}
-                        className={cn(element.attribs.class, className)}
-                    >
-                        {domToReact(element.children as DOMNode[])}
-                    </Component>
-                );
+                if (domNode.type === 'tag') {
+                    const element = domNode as Element;
+                    const Component = element.tagName as ElementType;
+                    const attrs = attributesToProps(element.attribs);
+                    return (
+                        <Component
+                            {...attrs}
+                            {...props}
+                            ref={ref}
+                            className={cn(element.attribs?.class, className)}
+                        >
+                            {domToReact(element.children as DOMNode[])}
+                        </Component>
+                    );
+                } else {
+                    return domToReact([domNode]);
+                }
             },
         });
     },
